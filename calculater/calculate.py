c0b63b79a653f757c24b668b772bbbeb8b4128bf
jihuuuu <jihuu2806@gmail.com>
2025-03-17 14:44:56 +0900

    FIX : Modify Data Type

--- calculater/calculate.py
+++ calculater/calculate.py
@@ -1,25 +1,25 @@
 def plus(a,b):
-    return a +b
+    return a + b
 def minus(a,b):
-    return a-b 
+    return a - b 
 
 def mul(a, b) :
-    return a*b
+    return a * b
 
 def divide(a,b) :
-    return a/b
+    return a / b
 
 if __name__ == '__main__' :
 
     ### 사용자 입력
     print('\n첫번째 숫자를 입력하세요.')
-    input1 = input('입력: ')
+    input1 = int(input('입력: '))
 
     print('\n원하는 사칙연산 기호 중 하나를 선택하세요. (+, -, *, /)')
     act = input('기호: ')
 
     print('\n두번째 숫자를 입력하세요.')
-    input2 = input('입력: ')
+    input2 = int(input('입력: '))
 
     if act == '+':
         result = plus(input1, input2)
@@ -30,4 +30,4 @@ if __name__ == '__main__' :
     elif act == '/':
         result = divide(input1, input2)
 
-    print('\n사칙연산 결과는 (result)입니다.')
+    print('\n사칙연산 결과는 ', result, '입니다.')
